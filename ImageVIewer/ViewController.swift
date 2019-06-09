d277fac68c3b6e7c0deaca237b01f58de41a9382
Vishal Lakshmi <vishal.rox26@gmail.com>
2019-06-09 09:11:32 -0700

    Initial Commit

---
+++ ImageVIewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageVIewer
+//
+//  Created by Vishal Lakshminarayanappa on 6/9/19.
+//  Copyright © 2019 Vishal Lakshminarayanappa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
